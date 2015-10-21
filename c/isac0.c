7077be7ae171c667b20e667eaf43785ac3128173
Linus Lee <linus@thelifelongtraveler.com>
2015-10-21 00:18:25 -0400

    Work done in C port

--- c/isac0.c
+++ c/isac0.c
@@ -1,6 +1,59 @@
-// C version of the Voyage functional encryption suite
+// C port of the Voyage functional encryption suite
+// outputs to a .veb (voyage encrypted binary) file
 
 #include "stdio.h"
+
+char repeat() {
+    // repeat the repeated string
+    // string manipulation function
+    return char[64] // of some kind
+}
+
+char processing[64]; // keeping this as a 64-character VEB for now.
+processing = ;// stuff
+
+int nbit;
+nbit = 32;
+
+char zerofiller;
+zerofiller = repeat("0", nbit);
+
+char onefiller;
+onefiller = repeat("1", nbit);
+
+char splitByN(char string) {
+    // equiv. to string.match(rset(n));
+    return char[64];
+}
+
+char encode(char string) {
+    // convert to binary string
+    return char binary;
+}
+
+char decode(char binary) {
+    // convert binary to char string
+    return char string;
+}
+
+// G elements
+char adder(int n, bool dir) {
+    // adder or rAdder, with dir = true being adder and the other being rAdder
+}
+
+char cycle(int n, bool dir) {
+
+}
+
+char reverse(int n, bool dir) {
+
+}
+
+char split(int n, bool dir) {
+
+}
+
+
 
 int main(void) {
     setvbuf(stdout, NULL, _IONBF, 0);
